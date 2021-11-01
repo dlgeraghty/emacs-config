a989f19c87ae1dd8cbe8db5b386b82306c0e1c0a
david <36100125+dlgeraghty@users.noreply.github.com>
2021-11-01 23:02:32 +0100

    initial basic config

---
+++ init.el
@@ -0,0 +1,55 @@
+
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)	; disable scrollbar
+(tool-bar-mode -1)	; disable toolbar
+(tooltip-mode -1)	; disable tooltip
+(set-fringe-mode 10)	; give room
+
+(menu-bar-mode -1)	; disable the menu bar
+
+;; set visible bell
+(setq visible-bell t)
+
+(load-theme 'wombat)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Packages stuff
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initializa use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package counsel)
+
