5a15874312694fbdeb62156401675adc01c2dab0
david <36100125+dlgeraghty@users.noreply.github.com>
2021-11-11 10:23:41 +0100

    update init.el according to Emacs.org tangle

--- init.el
+++ init.el
@@ -1,23 +1,3 @@
-
-(setq inhibit-startup-message t)
-
-(scroll-bar-mode -1)	; disable scrollbar
-(tool-bar-mode -1)	; disable toolbar
-(tooltip-mode -1)	; disable tooltip
-(set-fringe-mode 10)	; give room
-
-(menu-bar-mode -1)	; disable the menu bar
-
-;; set visible bell
-(setq visible-bell t)
-
-;;set fonts: be sure to have these fonts installed on your system!
-(set-face-attribute 'default nil :font "Inconsolata" :height 120)
-(set-face-attribute 'fixed-pitch nil :font "Inconsolata" :height 150)
-(set-face-attribute 'variable-pitch nil :font "Roboto" :height 175 :weight 'regular)
-
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
 ;; Packages stuff
 (require 'package)
 
@@ -36,41 +16,36 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(column-number-mode)
-(global-display-line-numbers-mode t)
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)	; disable scrollbar
+(tool-bar-mode -1)	; disable toolbar
+(tooltip-mode -1)	; disable tooltip
+(set-fringe-mode 10)	; give room
+
+(menu-bar-mode -1)	; disable the menu bar
+
+; set visible bell
+(setq visible-bell t)
+
+;;;set fonts: be sure to have these fonts installed on your system!
+(set-face-attribute 'default nil :font "Inconsolata" :height 120)
+(set-face-attribute 'fixed-pitch nil :font "Inconsolata" :height 150)
+(set-face-attribute 'variable-pitch nil :font "Roboto" :height 175 :weight 'regular)
 
 ;; Set frame transparency
-(set-frame-parameter (selected-frame) 'alpha '(90 . 90))
+(set-frame-parameter (selected-frame) 'alpha '(95 . 95))
 
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(column-number-mode)
+(global-display-line-numbers-mode t)
 ;; disable line numbers for some modes
 (dolist (mode '(org-mode-hook
-		term-mode-hook
-		shell-mode-hook
-		eshell-mode-hook))
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook
+                vterm-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
-
-(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
-
-(use-package counsel)
 
 (use-package doom-modeline
   :ensure t
@@ -79,14 +54,26 @@
 (use-package doom-themes :defer t)
 (load-theme 'doom-palenight t)
 
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
+(use-package ivy
+:diminish
+:bind (("C-s" . swiper)
+        :map ivy-minibuffer-map
+        ("TAB" . ivy-alt-done)
+        ("C-l" . ivy-alt-done)
+        ("C-j" . ivy-next-line)
+        ("C-k" . ivy-previous-line)
+        :map ivy-switch-buffer-map
+        ("C-k" . ivy-previous-line)
+        ("C-l" . ivy-done)
+        ("C-d" . ivy-switch-buffer-kill)
+        :map ivy-reverse-i-search-map
+        ("C-k" . ivy-previous-line)
+        ("C-d" . ivy-reverse-i-search-kill))
+:config
+(ivy-mode 1))
 
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
+(use-package counsel)
+
 
 (use-package ivy-rich
   :init
@@ -99,16 +86,8 @@
 	 :map minibuffer-local-map
 	 ("C-f" . 'counsel-minibuffer-history)))
 
-(use-package general
-  :config
-  (general-create-definer dvd/leader-keys
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "C-SPC")
-
-  (dvd/leader-keys
-    "t"  '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "toggles")))
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package evil
   :init
@@ -128,25 +107,30 @@
   :config
   (evil-collection-init))
 
+(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+(use-package general
+  :config
+  (general-create-definer dvd/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (dvd/leader-keys
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "toggles")))
+
 (defun dvd/org-mode-setup()
   (org-indent-mode)
   (variable-pitch-mode 1)
   (auto-fill-mode 0)
   (visual-line-mode 1)
-  (setq evil-auto-indent nil))
-
-(use-package org
-  :hook (org-mode . dvd/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▾"
-	org-hide-emphasis-markers t))
-
-
-(use-package org-bullets
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
+  (setq evil-auto-indent nil)
+  (setq org-startup-indented t))
 (defun efs/org-font-setup ()
   ;; Replace list hyphen with dot
   (font-lock-add-keywords 'org-mode
@@ -162,17 +146,29 @@
 		    (org-level-8 . 1.1)))
     (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
 
-(set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
-(set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
-(set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
-(set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
-(set-face-attribute 'line-number nil :inherit 'fixed-pitch)
-(set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch)
+    (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+    (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+    (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+    (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+    (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+    (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
+    (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
+    (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . dvd/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+	org-hide-emphasis-markers t)
+  (efs/org-font-setup))
+
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 (defun efs/org-mode-visual-fill()
   (setq visual-fill-column-width 100
@@ -182,3 +178,38 @@
 (use-package visual-fill-column
   :hook (org-mode . efs/org-mode-visual-fill))
 
+;; snippet to be able to just write <el + tab and get a source block for emacs lisp
+(require 'org-tempo)
+
+(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("py" . "src python"))
+(add-to-list 'org-structure-template-alist '("ht" . "src html"))
+(add-to-list 'org-structure-template-alist '("vh" . "src vhdl"))
+
+;; snippet to add python to the org-block executables:
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (python . t)
+   (shell . t)))
+
+;;caution!! this snippet will stop asking for confirmation when running a block of code in org mode!
+(setq org-confirm-babel-evaluate nil)
+
+;; disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+    (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Automatically tangle our Emacs.org config file when we save it
+(defun efs/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "/home/david/.emacs.d/Emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
